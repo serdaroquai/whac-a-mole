e3b5f9da4945d138985fea80662f051b94c54bab
serdaroquai <baykan.serdar@gmail.com>
2019-10-08 01:05:52 +0300

    Updates

---
+++ precache-manifest.cce2e9a49724a944b1ed0e865de96d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0269d6d18a3f20ab9a8d117d1d698e45",
+    "url": "/whac-a-mole/index.html"
+  },
+  {
+    "revision": "9f0ffb2aed02686a6a00",
+    "url": "/whac-a-mole/static/css/main.eee75ea5.chunk.css"
+  },
+  {
+    "revision": "68212d9514873ec50566",
+    "url": "/whac-a-mole/static/js/2.2a46379c.chunk.js"
+  },
+  {
+    "revision": "9f0ffb2aed02686a6a00",
+    "url": "/whac-a-mole/static/js/main.78644372.chunk.js"
+  },
+  {
+    "revision": "36bac8292e4d2f13d1da",
+    "url": "/whac-a-mole/static/js/runtime-main.6ef9e6e1.js"
+  }
+]);
